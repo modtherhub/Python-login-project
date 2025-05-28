5fcb9f68b463f8deea8466dbcf2ded619346e04f
modtherhub <moibhub@gmail.com>
2025-05-28 23:52:00 +0400

    Edit style

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import tkinter
 window = tkinter.Tk()
 window.title("Login Form")
 window.geometry('340x340')
+window.configure(bg="#333333")
 
-login_label = tkinter.Label(window, text="Login")
-username_label = tkinter.Label(window, text="Username")
-username_entry = tkinter.Entry(window,)
-password_label = tkinter.Label(window, text="Password")
-login_label = tkinter.Entry(window, show="*")
-login_button = tkinter.Button(window, text="Login")
+frame = tkinter.Frame(bg="#333333")
+
+login_label = tkinter.Label(frame, text="Login", bg="#333333", fg="#FFFFFF", font=("Arial", 30))
+username_label = tkinter.Label(frame, text="Username", bg="#333333", fg="#FFFFFF", font=("Arial", 16) )
+username_entry = tkinter.Entry(frame, font=("Arial", 16))
+password_label = tkinter.Label(frame, text="Password", bg="#333333", fg="#FFFFFF", font=("Arial", 16))
+password_entry = tkinter.Entry(frame, show="*", font=("Arial", 16))
+login_button = tkinter.Button(frame, text="Login", bg="#817272", fg="#FFFFFF", font=("Arial", 16))
 
 
-login_label.grid(row=0, column=0, columnspan=2)
-username_label.grid(row=1, column=0, )
-username_entry.grid(row=1, column=1, )
-password_label.grid(row=2, column=0, )
-login_label.grid(row=2, column=1, )
-login_button.grid(row=3, column=0, columnspan=2)
+login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady = 40)
+username_label.grid(row=1, column=0, padx = 10)
+username_entry.grid(row=1, column=1, pady = 10, padx = 10)
+password_label.grid(row=2, column=0, padx = 10)
+password_entry.grid(row=2, column=1, pady = 10, padx = 10)
+login_button.grid(row=3, column=0, columnspan=2, pady = 20)
+
+frame.pack()
 
 window.mainloop()
